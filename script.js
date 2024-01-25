aba63c493c75a7c9d8434b7def16a5b1cabdb4de
Makeadrawing <157713751+Makeadrawing@users.noreply.github.com>
2024-01-25 17:09:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const canvas = document.getElementById('drawingCanvas');
 const context = canvas.getContext('2d');
 const colorPicker = document.getElementById('color');
 const eraserCheckbox = document.getElementById('eraser');
+const undoButton = document.getElementById('undo');
 
 let isDrawing = false;
 let isErasing = false;
+let drawingMoves = [];  // Tableau pour stocker les mouvements
 
 canvas.addEventListener('mousedown', startDrawing);
 canvas.addEventListener('mousemove', draw);
@@ -13,6 +15,9 @@ canvas.addEventListener('mouseup', stopDrawing);
 function startDrawing(e) {
     isDrawing = true;
     draw(e);
+
+    // Commencer un nouveau mouvement
+    drawingMoves.push([]);
 }
 
 function draw(e) {
@@ -29,6 +34,9 @@ function draw(e) {
     context.stroke();
     context.beginPath();
     context.moveTo(mouseX, mouseY);
+
+    // Ajouter le point au mouvement actuel
+    drawingMoves[drawingMoves.length - 1].push({ x: mouseX, y: mouseY });
 }
 
 function stopDrawing() {
@@ -54,6 +62,26 @@ function shareDrawing() {
     alert('Sharing functionality not implemented yet.');
 }
 
+undoButton.addEventListener('click', undoDrawing);
+
 function undoDrawing() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    if (drawingMoves.length > 0) {
+        // Retirer le dernier mouvement
+        drawingMoves.pop();
+
+        // Effacer le canevas
+        context.clearRect(0, 0, canvas.width, canvas.height);
+
+        // Redessiner en utilisant les mouvements restants
+        for (const moves of drawingMoves) {
+            if (moves.length > 0) {
+                context.beginPath();
+                context.moveTo(moves[0].x, moves[0].y);
+                for (const move of moves) {
+                    context.lineTo(move.x, move.y);
+                }
+                context.stroke();
+            }
+        }
+    }
 }
